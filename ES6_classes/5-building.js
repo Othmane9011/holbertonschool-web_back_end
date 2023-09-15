53f3f3cea1283afb97fb5709a9079628b7082977
othmane9011 <5650@holbertonstudents.com>
2023-09-15 10:12:10 +0200

    correction

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 class Building {
-  constructor(sqft) {
-    if (
-      this.constructor !== Building
+    constructor(sqft) {
+      if (
+        this.constructor !== Building
         && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+      ) {
+        throw Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+  
+      this._sqft = sqft;
     }
-
-    this._sqft = sqft;
+  
+    get sqft() {
+      return this._sqft;
+    }
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
-
-export default Building;
+  
+  export default Building;
